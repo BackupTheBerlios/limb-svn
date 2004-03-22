13019c35fa915ea9795be1d03d40a64f9b1de1b5
mike <mike@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-03-22 13:57:14 +0000

    -- catalog module added

---
+++ trunk/core/model/site_objects/catalog_object.class.php
@@ -0,0 +1,31 @@
+<?php
+/**********************************************************************************
+* Copyright 2004 BIT, Ltd. http://www.0x00.ru, mailto: bit@0x00.ru
+*
+* Released under the LGPL license (http://www.gnu.org/copyleft/lesser.html)
+***********************************************************************************
+*
+* $Id$
+*
+***********************************************************************************/ 
+require_once(LIMB_DIR . 'core/model/site_objects/content_object.class.php');
+
+class catalog_object extends content_object
+{
+	function catalog_object()
+	{
+		parent :: content_object();
+	}
+	
+
+	function _define_class_properties()
+	{
+		return array(
+			'ordr' => 1,
+			'can_be_parent' => 0,
+			'controller_class_name' => 'catalog_object_controller',
+		);
+	}
+}
+
+?>
---
+++ trunk/tests/cases/site_objects_optional/catalog_object/catalog_object_manipulation.test.php
@@ -0,0 +1,41 @@
+<?php
+/**********************************************************************************
+* Copyright 2004 BIT, Ltd. http://www.0x00.ru, mailto: bit@0x00.ru
+*
+* Released under the LGPL license (http://www.gnu.org/copyleft/lesser.html)
+***********************************************************************************
+*
+* $Id: school_news_object_manipulation.test.php 21 2004-02-29 18:59:25Z server $
+*
+***********************************************************************************/ 
+
+require_once(LIMB_DIR . '/tests/cases/site_objects/_content_object_template.test.php');
+
+class test_catalog_object_manipulation extends test_content_object_template 
+{  	
+  function test_catalog_object_manipulation() 
+  {
+  	parent :: test_content_object_template();
+  }
+
+  function & _create_site_object()
+  {
+		$obj =& site_object_factory :: create('catalog_object');
+  	return $obj;
+  }
+  
+  function _set_object_initial_attributes()
+  {
+  	$this->object->set_attribute('annotaion', 'Catalog object annottation');
+  	$this->object->set_attribute('content', 'Catalog object content');  	
+  	$this->object->set_attribute('image_id', '123');
+  }
+	
+	function _set_object_secondary_update_attributes()
+	{
+  	$this->object->set_attribute('annotaion', 'Catalog object annottation');
+  	$this->object->set_attribute('content', 'Catalog object content');  	
+  	$this->object->set_attribute('image_id', '123');
+	}
+}
+?>
