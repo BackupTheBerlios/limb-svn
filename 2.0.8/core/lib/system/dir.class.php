c3756d5d775b0ebbdaaa0eae5c453085f81edbb7
server <server@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-05-04 09:46:02 +0000

    -- clearstatcache call added when removing or copying a directory

--- 2.0.8/core/lib/system/dir.class.php
+++ 2.0.8/core/lib/system/dir.class.php
@@ -127,11 +127,12 @@ class dir
 	function rm($dir)
 	{
 		dir :: _do_rm(dir :: chop($dir), dir :: separator());
+		clearstatcache();
 	}
 	
 	function _do_rm($dir, $separator)
 	{
-    if (is_dir($dir) && $handle = opendir($dir))
+    if (is_dir($dir) && ($handle = opendir($dir)))
     {
       while(($file = readdir($handle)) !== false)
       {
@@ -207,6 +208,9 @@ class dir
         }
       }
     }
+    if($total_items)
+    	clearstatcache();
+    	
 		return $total_items;
   }
 	
