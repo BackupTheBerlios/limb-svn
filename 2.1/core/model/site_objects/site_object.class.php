de1a107f4061022d36d150c3a57d27f731088542
server <server@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-05-05 06:58:57 +0000

    -- site object now uses site_object_controller

--- 2.1/core/model/site_objects/site_object.class.php
+++ 2.1/core/model/site_objects/site_object.class.php
@@ -54,6 +54,7 @@ class site_object extends object
 			'class_ordr' => 1,
 			'can_be_parent' => 1,
 			'icon' => '/shared/images/generic.gif',
+			'controller_class_name' => 'site_object_controller'
 		);
 	}
 	
