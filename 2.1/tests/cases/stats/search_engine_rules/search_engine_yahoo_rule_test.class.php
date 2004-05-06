3f5dbb0548fa5c77c1fb806c223f953dfd989582
server <server@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-05-06 09:40:12 +0000

    -- FINAL(previous one was unclean due to some svn glitch)!!! major refactoring of the test cases:  * .test.php => _test.class.php

---
+++ 2.1/tests/cases/stats/search_engine_rules/search_engine_yahoo_rule_test.class.php
@@ -0,0 +1,51 @@
+<?php
+/**********************************************************************************
+* Copyright 2004 BIT, Ltd. http://www.0x00.ru, mailto: bit@0x00.ru
+*
+* Released under the LGPL license (http://www.gnu.org/copyleft/lesser.html)
+***********************************************************************************
+*
+* $Id$
+*
+***********************************************************************************/ 
+require_once(LIMB_DIR . '/core/model/stats/search_engine_rules/search_engine_yahoo_rule.class.php');
+
+class search_engine_yahoo_rule_test extends UnitTestCase 
+{
+  var $rule = null;
+	
+  function search_engine_yahoo_rule_test() 
+  {
+  	parent :: UnitTestCase();
+  }
+  
+  function setUp()
+  {
+   	$this->rule = new search_engine_yahoo_rule();
+  }
+  
+  function test_name()
+  {
+  	$this->assertEqual('yahoo', $this->rule->get_engine_name());
+  }
+   
+  function test_match_ru_true()
+  {
+  	$this->assertTrue($this->rule->match(urldecode('http://search.yahoo.com/search?p=%D0%BF%D1%80%D0%B8%D0%B2%D0%B5%D1%82+%D0%BF%D1%80%D0%B8%D0%B2%D0%B5%D1%82&ei=UTF-8&fr=fp-tab-web-t&cop=mss&tab=')));
+  	$this->assertEqual('привет привет', $this->rule->get_matching_phrase());
+  } 
+  
+  function test_match_second_ru_true()
+  {
+  	$this->assertTrue($this->rule->match(urldecode('http://search.yahoo.com/search?p=%d0%bf%d1%80%d0%b8%d0%b2%d0%b5%d1%82&ei=UTF-8&cop=mss&fr=fp-tab-web-t&b=21')));
+  	$this->assertEqual('привет', $this->rule->get_matching_phrase());
+  } 
+  
+  function test_match_eng_true()
+  {
+  	$this->assertTrue($this->rule->match(urldecode('http://search.yahoo.com/search?p=wow&ei=UTF-8&n=20&fl=0&fr=fp-tab-web-t&b=181')));
+  	$this->assertEqual('wow', $this->rule->get_matching_phrase());
+  }
+}
+
+?>
---
+++ 2.1/tests/cases/validation/rules/url_rule_test.class.php
@@ -0,0 +1,45 @@
+<?php
+/**********************************************************************************
+* Copyright 2004 BIT, Ltd. http://www.0x00.ru, mailto: bit@0x00.ru
+*
+* Released under the LGPL license (http://www.gnu.org/copyleft/lesser.html)
+***********************************************************************************
+*
+* $Id$
+*
+***********************************************************************************/ 
+require_once(LIMB_DIR . 'core/lib/util/dataspace.class.php');
+require_once(LIMB_DIR . 'core/lib/validators/rules/url_rule.class.php');
+
+class url_rule_test extends single_field_rule_test
+{
+
+	function test_url_rule_valid()
+	{
+		$this->validator->add_rule(new url_rule('test'));
+
+		$data =& new dataspace();
+		$data->set('test', 'http://wow.com.dot/this/a/valid/url');
+
+		$this->error_list->expectNever('add_error');
+
+		$this->validator->validate($data);
+		$this->assertTrue($this->validator->is_valid());
+	} 
+	
+	function test_url_rule_invalid()
+	{
+		$this->validator->add_rule(new url_rule('testfield'));
+
+		$data =& new dataspace();
+		$data->set('testfield', '://not/a/valid/url');
+
+		$this->error_list->expectOnce('add_error', array('testfield', 'BAD_URL', array()));
+
+		$this->validator->validate($data);
+		$this->assertFalse($this->validator->is_valid());
+	}
+
+} 
+
+?>
