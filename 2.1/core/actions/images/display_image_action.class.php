9fe4fc0571654a4696852739825822626d7308f6
pachanga <pachanga@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-06-02 10:16:16 +0000

    -- for speed issues with images and files all dont track responses replaced with mere exit()

--- 2.1/core/actions/images/display_image_action.class.php
+++ 2.1/core/actions/images/display_image_action.class.php
@@ -45,7 +45,7 @@ class display_image_action extends action
 			if($variation == 'original')
 				return new exit_response(RESPONSE_STATUS_FAILURE);
 			else
-				return new dont_track_response();
+				exit();//for speed
 
 		}		
 				
@@ -56,7 +56,7 @@ class display_image_action extends action
 			if($variation == 'original')
 				return new exit_response(RESPONSE_STATUS_FAILURE);
 			else
-				return new exit_response(RESPONSE_STATUS_DONT_TRACK);
+				exit();//for speed
 		}		
 				
 		if (isset($_SERVER['HTTP_IF_NONE_MATCH']) && $_SERVER['HTTP_IF_NONE_MATCH'] == $image['etag'])
@@ -81,7 +81,7 @@ class display_image_action extends action
 		if($variation == 'original')
 			return new exit_response();
 		else
-			return new exit_response(RESPONSE_STATUS_DONT_TRACK);
+			exit();//for speed
 	}
 }
 
