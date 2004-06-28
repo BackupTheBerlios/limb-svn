77db5c9ddca7a2c4c0e2d60aaa9b27a2d434e323
pachanga <pachanga@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-06-28 15:16:55 +0000

    -- small require_once bug fixed

--- trunk/counter.php
+++ trunk/counter.php
@@ -9,7 +9,6 @@
 *
 ***********************************************************************************/ 
 require_once('setup.php');
-require_once(LIMB_DIR . 'core/lib/http/http_request.inc.php');
 require_once(LIMB_DIR . 'core/lib/db/db_factory.class.php');
 
 function get_counter_record()
@@ -17,19 +16,6 @@ function get_counter_record()
 	$db =& db_factory :: instance();
 	$db->sql_select('sys_stat_counter', '*');
 	return $db->fetch_row();
-}
-
-function register_client_info()
-{
-	//collecting js client specific stuff
-	$script_language = isset($_REQUEST['sl']) ? $_REQUEST['sl'] : '?';
-	$cookie_enabled = isset($_REQUEST['c']) ? $_REQUEST['c'] : '?';
-	$java_enabled = isset($_REQUEST['j']) ? $_REQUEST['j'] : '?';
-	$screen_size = isset($_REQUEST['wh']) ? $_REQUEST['wh'] : '?';
-	$screen_depth = isset($_REQUEST['px']) ? $_REQUEST['px'] : '?';
-	$time_offset = isset($_REQUEST['t']) ? $_REQUEST['t'] : '?';
-	
-	//....
 }
 						
 ?>
