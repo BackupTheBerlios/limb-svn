594bc8220ce04ad5d4d7a15fc6fa50cdbf69271f
seregalimb <seregalimb@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-07-19 06:31:19 +0000

    -- core:STATUS tag does not require constant to be defined at compile time.

--- 2.2/core/template/tags/core/status.tag.php
+++ 2.2/core/template/tags/core/status.tag.php
@@ -40,14 +40,6 @@ class core_status_tag extends compiler_directive_tag
 					'line' => $this->starting_line_no));
 		} 
 		
-		if(!defined($this->attributes['name']))
-		{
-			error('CONSTNOTDEFINED', __FILE__ . ' : ' . __LINE__ . ' : ' .  __FUNCTION__, array('tag' => $this->tag,
-					'const' => $this->attributes['name'],
-					'file' => $this->source_file,
-					'line' => $this->starting_line_no));		
-		}
-
 		$this->const = $this->attributes['name'];
 
 		return PARSER_REQUIRE_PARSING;
