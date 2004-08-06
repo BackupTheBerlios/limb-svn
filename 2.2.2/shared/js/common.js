a9cee1216c45bc5c30215afd14b901c94accc7ec
seregalimb <seregalimb@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-08-06 10:56:16 +0000

    -- minor changes

--- 2.2.2/shared/js/common.js
+++ 2.2.2/shared/js/common.js
@@ -442,13 +442,11 @@ function add_form_hidden_parameter(form, parameter, val)
 	hidden = document.getElementById(parameter + '_hidden_parameter');
 	if(hidden)
 	{
-		alert(1);
 		hidden.value = val;
 		form.appendChild(hidden);
 	}	
 	else
 	{			
-		alert(2);
 		hidden = document.createElement('INPUT');
 		hidden.id = parameter + '_hidden_parameter';
 		hidden.type = 'hidden';
