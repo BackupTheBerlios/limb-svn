72198eeb4131fcb2d8ab10ba721bd4a3eb4e8473
pachanga <pachanga@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-08-31 12:33:47 +0000

    -- specific actions removed, only base ones remained

--- trunk/core/actions/action_factory.class.php
+++ trunk/core/actions/action_factory.class.php
@@ -12,11 +12,7 @@ require_once(LIMB_DIR . 'core/lib/debug/debug.class.php');
 require_once(LIMB_DIR . 'core/actions/empty_action.class.php');
 
 class action_factory
-{
-	function action_factory()
-	{
-	}
-	
+{	
 	function get_class_name($class_path)
 	{
 		$pos = strrpos($class_path, '/');
@@ -32,18 +28,18 @@ class action_factory
 	function & create($class_path)
 	{
 		$class_name = action_factory :: get_class_name($class_path);
-				
-		if (file_exists(PROJECT_DIR . '/core/actions/' . $class_path . '.class.php')) 
-			$full_path = PROJECT_DIR . '/core/actions/' . $class_path . '.class.php';
-		elseif(file_exists(LIMB_DIR . '/core/actions/' . $class_path . '.class.php'))
-			$full_path = LIMB_DIR . '/core/actions/' . $class_path . '.class.php';
-		else
+		
+		if(!class_exists($class_name))
 		{
-			debug :: write_error('action not found', __FILE__ . ' : ' . __LINE__ . ' : ' .  __FUNCTION__, array('class_path' => $class_path));
-			return new empty_action();
-		}
-			
-		include_once($full_path);
+  		$resolver =& get_file_resolver('action');
+  		resolve_handle($resolver);
+  		
+  		if(!$full_path = $resolver->resolve($class_path))
+  			return new empty_action();
+  			
+  		include_once($full_path);
+  	}
+  	
 	  $action =& new $class_name();
 	  
 	  if (!is_object($action))
@@ -58,7 +54,7 @@ class action_factory
 		}
 	  
 	  return $action;
-	}
+	}	
 }
 
 
