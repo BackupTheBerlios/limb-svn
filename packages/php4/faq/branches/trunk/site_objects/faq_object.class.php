945fd78c3e185d87af0ce3d61cdb593906b319f4
pachanga <pachanga@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-09-03 14:21:46 +0000

    -- initial revision of LIMB_FAQ package

---
+++ packages/php4/faq/branches/trunk/site_objects/faq_object.class.php
@@ -0,0 +1,36 @@
+<?php
+/**********************************************************************************
+* Copyright 2004 BIT, Ltd. http://www.0x00.ru, mailto: bit@0x00.ru
+*
+* Released under the LGPL license (http://www.gnu.org/copyleft/lesser.html)
+***********************************************************************************
+*
+* $Id$
+*
+***********************************************************************************/ 
+require_once(LIMB_DIR . 'class/core/site_objects/content_object.class.php');
+
+class faq_object extends content_object
+{
+	function _define_attributes_definition()
+	{
+		return complex_array :: array_merge(
+				parent :: _define_attributes_definition(),
+				array(
+				'question' => array('search' => true),
+				'answer' => array('search' => true)
+				));
+	}
+	
+	function _define_class_properties()
+	{
+		return array(
+			'class_ordr' => 2,
+			'can_be_parent' => 0,
+			'controller_class_name' => 'faq_object_controller',
+			'auto_identifier' => true
+		);
+	}
+}
+
+?>
