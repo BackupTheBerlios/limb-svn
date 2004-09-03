792b0ae52596f7642a9ccbd28929788ca2a8f49b
pachanga <pachanga@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-09-03 12:04:35 +0000

    -- initial revision of LIMB_STATS package

---
+++ packages/php4/stats/branches/trunk/datasources/stats_routes_list_datasource.class.php
@@ -0,0 +1,73 @@
+<?php
+/**********************************************************************************
+* Copyright 2004 BIT, Ltd. http://www.0x00.ru, mailto: bit@0x00.ru
+*
+* Released under the LGPL license (http://www.gnu.org/copyleft/lesser.html)
+***********************************************************************************
+*
+* $Id$
+*
+***********************************************************************************/ 
+
+require_once(LIMB_DIR . 'class/datasources/datasource.class.php');
+require_once(LIMB_DIR . 'class/stats/stats_routes_report.class.php');
+
+class stats_routes_list_datasource extends datasource
+{		
+	var $stats_routes_report = null;
+	
+	function stats_routes_list_datasource()
+	{
+		$this->stats_report =& new stats_routes_report();
+		
+		parent :: datasource();		
+	}
+
+	function & get_dataset(&$counter, $params=array())
+	{		
+		$this->_configure_filters();
+		
+		$counter = $this->stats_report->fetch_count($params);
+		$arr = $this->stats_report->fetch($params);
+		
+		$arr = $this->_process_result_array($arr);
+		
+		return new array_dataset($arr);
+	}
+	
+	function _process_result_array($arr)		
+	{
+		return $arr;
+	}	
+	
+	function _configure_filters()
+	{
+	  $request = request :: instance();
+	
+		$this->_set_period_filter($request);
+	}
+		
+	function _set_period_filter(&$request)
+	{
+		$locale =& locale :: instance();
+		$start_date = new date();
+		$start_date->set_hour(0);
+		$start_date->set_minute(0);
+		$start_date->set_second(0);
+
+	  if ($stats_start_date = $request->get('stats_start_date'))
+			$start_date->set_by_string($stats_start_date, $locale->get_short_date_time_format());
+		
+		$finish_date = new date();
+
+	  if ($stats_finish_date = $request->get('stats_finish_date'))
+			$finish_date->set_by_string($stats_finish_date, $locale->get_short_date_time_format());
+
+		$finish_date->set_hour(23);
+		$finish_date->set_minute(59);
+		$finish_date->set_second(59);
+		
+		$this->stats_report->set_period_filter($start_date, $finish_date);
+	}
+}
+?>
