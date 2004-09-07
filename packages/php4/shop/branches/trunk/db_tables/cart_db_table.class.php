64569149e81fdea9fcc3296958af38a3c40e464d
pachanga <pachanga@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-09-07 14:00:26 +0000

    -- LIMB_SHOP package fixes

---
+++ packages/php4/shop/branches/trunk/db_tables/cart_db_table.class.php
@@ -0,0 +1,27 @@
+<?php
+/**********************************************************************************
+* Copyright 2004 BIT, Ltd. http://www.0x00.ru, mailto: bit@0x00.ru
+*
+* Released under the LGPL license (http://www.gnu.org/copyleft/lesser.html)
+***********************************************************************************
+*
+* $Id$
+*
+***********************************************************************************/ 
+require_once(LIMB_DIR  . '/class/lib/db/db_table.class.php');
+
+class cart_db_table extends db_table
+{
+  function _define_columns()
+  {
+  	return array(
+      'id' => array('type' => 'numeric'),
+      'user_id' => array('type' => 'numeric'),
+      'cart_id' => '',
+      'cart_items' => array('type' => 'clob'),
+      'last_activity_time' => array('type' => 'numeric')
+    );
+  }
+}
+
+?>
