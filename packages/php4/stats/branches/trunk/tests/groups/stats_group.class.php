8261833d645bf25b124ee6fb041c254021c20986
pachanga <pachanga@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-09-07 13:56:40 +0000

    -- fixed LIMB_STATS package tests

---
+++ packages/php4/stats/branches/trunk/tests/groups/stats_group.class.php
@@ -0,0 +1,23 @@
+<?php
+/**********************************************************************************
+* Copyright 2004 BIT, Ltd. http://www.0x00.ru, mailto: bit@0x00.ru
+*
+* Released under the LGPL license (http://www.gnu.org/copyleft/lesser.html)
+***********************************************************************************
+*
+* $Id$
+*
+***********************************************************************************/ 
+class stats_group extends LimbGroupTest 
+{
+	function stats_group() 
+	{
+	  $this->LimbGroupTest('stats tests');
+	}
+	
+	function getTestCasesHandles()
+	{
+	  return TestManager::getTestCasesHandlesFromDirectory(dirname(__FILE__) . '/../cases/stats');
+	}
+}
+?>
