ae280606f4102e3d7fbcee92ec4ea082810909ba
pachanga <pachanga@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-09-08 15:44:06 +0000

    -- php5 packages copy

---
+++ packages/php5/faq/branches/trunk/site_objects/faq_folder_container.class.php
@@ -0,0 +1,27 @@
+<?php
+/**********************************************************************************
+* Copyright 2004 BIT, Ltd. http://www.0x00.ru, mailto: bit@0x00.ru
+*
+* Released under the LGPL license (http://www.gnu.org/copyleft/lesser.html)
+***********************************************************************************
+*
+* $Id$
+*
+***********************************************************************************/ 
+require_once(LIMB_DIR . 'class/core/site_objects/site_object.class.php');
+
+class faq_folder_container extends site_object
+{
+	function _define_class_properties()
+	{
+		return array(
+			'class_ordr' => 0,
+			'can_be_parent' => 1,
+			'controller_class_name' => 'faq_folder_container_controller',
+			'icon' => '/shared/images/folder.gif'
+		);
+	}
+
+}
+
+?>
