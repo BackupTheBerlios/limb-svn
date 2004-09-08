1286bf1809e698b18955d310c9072df63c8aaf42
pachanga <pachanga@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-09-08 15:45:07 +0000

    -- php5 packages copy

---
+++ packages/php5/news/branches/trunk/actions/news/edit_news_action.class.php
@@ -0,0 +1,48 @@
+<?php
+/**********************************************************************************
+* Copyright 2004 BIT, Ltd. http://www.0x00.ru, mailto: bit@0x00.ru
+*
+* Released under the LGPL license (http://www.gnu.org/copyleft/lesser.html)
+***********************************************************************************
+*
+* $Id$
+*
+***********************************************************************************/ 
+require_once(LIMB_DIR . 'class/core/actions//form_edit_site_object_action.class.php');
+
+class edit_news_action extends form_edit_site_object_action
+{
+	function _define_site_object_class_name()
+	{
+	  return 'news_object';
+	}  
+	  
+	function _define_dataspace_name()
+	{
+	  return 'news_form';
+	}
+  
+  function _define_datamap()
+	{
+	  return complex_array :: array_merge(
+	      parent :: _define_datamap(),
+	      array(
+  				'annotation' => 'annotation',
+  				'news_content' => 'content',
+  				'news_date' => 'news_date',
+	      )
+	  );     
+	}  
+	
+	function _init_validator()
+	{
+		parent :: _init_validator();
+		
+    $this->validator->add_rule($v1 = array(LIMB_DIR . 'class/validators/rules/required_rule', 'title'));
+    $this->validator->add_rule($v2 = array(LIMB_DIR . 'class/validators/rules/required_rule', 'annotation'));
+    $this->validator->add_rule($v3 = array(LIMB_DIR . 'class/validators/rules/required_rule', 'news_date'));
+    $this->validator->add_rule($v4 = array(LIMB_DIR . 'class/validators/rules/locale_date_rule', 'news_date'));
+	}
+}
+
+?>
