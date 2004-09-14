e360bddd7461a9321d2d57b1b51f935d3cf3a488
dbrain <dbrain@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-09-14 14:06:55 +0000

    -- control panel object added;

---
+++ php4/branches/2.2.2/core/model/site_objects/control_panel.class.php
@@ -0,0 +1,25 @@
+<?php
+/**********************************************************************************
+* Copyright 2004 BIT, Ltd. http://www.0x00.ru, mailto: bit@0x00.ru
+*
+* Released under the LGPL license (http://www.gnu.org/copyleft/lesser.html)
+***********************************************************************************
+*
+* $Id: admin_page.class.php 324 2004-06-11 13:05:50Z pachanga $
+*
+***********************************************************************************/ 
+require_once(LIMB_DIR . 'core/model/site_objects/site_object.class.php');
+
+class control_panel extends site_object
+{
+	function _define_class_properties()
+	{
+		return array(
+			'class_ordr' => 0,
+			'can_be_parent' => 0,
+			'controller_class_name' => 'control_panel_controller',
+		);
+	}
+}
+
+?>
