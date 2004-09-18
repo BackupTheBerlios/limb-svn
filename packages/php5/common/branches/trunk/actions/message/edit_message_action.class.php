13e9010872ecb0a6432859a3082e7c0557f3a005
pachanga <pachanga@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-09-18 14:43:42 +0000

    -- message actions added

---
+++ packages/php5/common/branches/trunk/actions/message/edit_message_action.class.php
@@ -0,0 +1,44 @@
+<?php
+/**********************************************************************************
+* Copyright 2004 BIT, Ltd. http://www.0x00.ru, mailto: bit@0x00.ru
+*
+* Released under the LGPL license (http://www.gnu.org/copyleft/lesser.html)
+***********************************************************************************
+*
+* $Id$
+*
+***********************************************************************************/ 
+require_once(LIMB_DIR . '/class/core/actions/form_edit_site_object_action.class.php');
+
+class edit_message_action extends form_edit_site_object_action
+{
+	protected function _define_site_object_class_name()
+	{
+	  return 'message';
+	}  
+	  
+	protected function _define_dataspace_name()
+	{
+	  return 'edit_message';
+	}
+  
+  protected function _define_datamap()
+	{
+	  return complex_array :: array_merge(
+	      parent :: _define_datamap(),
+	      array(
+  				'content' => 'content',
+	      )
+	  );     
+	}  
+	
+	protected function _init_validator()
+	{
+		parent :: _init_validator();
+
+    $this->validator->add_rule(array(LIMB_DIR . '/class/validators/rules/required_rule', 'title'));
+    $this->validator->add_rule(array(LIMB_DIR . '/class/validators/rules/required_rule', 'content'));
+	}
+}
+
+?>
