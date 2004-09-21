9af3099cfdc3d3f4a3d2a639b2273aa7dd54bfc8
pachanga <pachanga@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-09-21 06:14:36 +0000

    -- initial revision of const:OPTIONAL tag

---
+++ packages/php5/common/branches/trunk/template/tags/debug/const_optional.tag.php
@@ -0,0 +1,59 @@
+<?php
+/**********************************************************************************
+* Copyright 2004 BIT, Ltd. http://limb-project.com, mailto: limb@0x00.ru
+*
+* Released under the LGPL license (http://www.gnu.org/copyleft/lesser.html)
+***********************************************************************************
+*
+* $Id$
+*
+***********************************************************************************/
+
+class const_optional_tag_info
+{
+	public $tag = 'const:OPTIONAL';
+	public $end_tag = ENDTAG_REQUIRED;
+	public $tag_class = 'const_optional_tag';
+} 
+
+register_tag(new const_optional_tag_info());
+
+class const_optional_tag extends compiler_directive_tag
+{	
+  protected $const;
+  
+	public function pre_parse()
+	{
+		if (!isset($this->attributes['name']))
+		{
+			throw new WactException('missing required attribute', 
+					array('tag' => $this->tag,
+					'attribute' => 'name',
+					'file' => $this->source_file,
+					'line' => $this->starting_line_no));
+		} 
+		
+		$this->const = $this->attributes['name'];
+
+		return PARSER_REQUIRE_PARSING;
+	} 
+
+	public function pre_generate($code)
+	{
+		$value = 'true';
+		if (isset($this->attributes['value']) && !(boolean)$this->attributes['value'])
+			$value = 'false';
+
+		$code->write_php('if (defined("' . $this->const . '") && (constant("' . $this->const . '")) === ' . $value . ') {');
+		
+		parent::pre_generate($code);
+	}
+	
+	public function post_generate($code)
+	{
+	  parent::post_generate($code);
+		$code->write_php('}');		
+	}  	
+} 
+
+?>
