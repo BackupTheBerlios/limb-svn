46c349f1df43c1fa2bdea81883dce381f2b4266c
moltyaninov <moltyaninov@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-09-23 11:17:23 +0000

    -- hint javascript code moved to hint.js from tag; -- initial revision of dropdown jip actions;

--- php4/branches/2.2.3/core/template/tags/hint/hint.tag.php
+++ php4/branches/2.2.3/core/template/tags/hint/hint.tag.php
@@ -22,117 +22,11 @@ class hint_hint_tag extends compiler_directive_tag
 {
 	function pre_generate(&$code)
 	{
-	
-$js = <<<JS
-<script language="javascript">
-dom = (document.getElementById) ? true : false;
-nn4 = (document.hints) ? true : false;
-ie = (document.all) ? true : false;
-ie4 = ie && !dom;
-
-var current_hint_x = -1, current_hint_y = -1;
-var current_hint_id = null;
-
-//document.onmousemove = hint_mouse_move_handler; 
-add_event(document, 'mousemove', hint_mouse_move_handler);
-
-function hint_mouse_move_handler(e)
-{
-  if (dom)
-  {
-    current_hint_x = e.clientX;   
-    current_hint_y = e.clientY;
-  }
-  else if(ie4)
-  {
-    current_hint_x = e.x + document.body.scrollLeft;
-    current_hint_y = e.y + document.body.scrollTop;  
-  }
-  else if(nn4)
-  {
-    current_hint_x = e.pageX;
-    current_hint_y = e.pageY;
-  }
-  
-  on_hint_mouse_move();
-}
-
-function on_hint_mouse_move() 
-{
-  if(current_hint_id)
-  	move_hint(current_hint_id, current_hint_x+10, current_hint_y+10);
-}
-
-function show_hint(id) 
-{
-  if (dom) 
-  {
-  	document.getElementById(id).style.visibility = "visible";
-   	document.getElementById(id).style.display = '';
-  }
-  else if (ie4) 
-  {
-  	document.all[id].style.visibility = "visible";
-    document.all[id].style.display= '';
-  } 
-  else if (nn4) 
-  {
-  	document.hints[id].visibility = "show";
-    document.hints[id].display='';
-  }
-}
-
-function hide_hint(id) 
-{
-  if (dom) document.getElementById(id).style.visibility = "hidden";
-  else if (ie4) document.all[id].style.visibility = "hidden";
-  else if (nn4) document.hints[id].visibility = "hide";
-}
-
-function move_hint(id,x,y)
-{
-  if (dom)
-  {
-    h = document.getElementById(id);
-    h.style.left = x;
-    h.style.top = y;
-  }
-  else if(ie4)
-  {
-    document.all[id].left = x;
-    document.all[id].top = y;    
-  }
-  else if(nn4)
-  {
-    document.hints[id].left = x;
-    document.hints[id].top = y;
-  }
-}
-
-function start_hint(id)
-{
-  if(current_hint_id)
-  	hide_hint(current_hint_id);
-  
-  current_hint_id = id;
-  
-  move_hint(current_hint_id, current_hint_x+10, current_hint_y+10);
-  show_hint(current_hint_id);
-}
-
-function stop_hint()
-{
-  hide_hint(current_hint_id);
-  current_hint_id = null;
-}
-</script>
-JS;
-
 		parent :: pre_generate($code);
 		
 		if (!defined('HINT_SCRIPT_INCLUDED'))
 		{
-			$code->write_html($js);
+			$code->write_html("<script type='text/javascript' src='/shared/js/hint.js'></script>");
 			define('HINT_SCRIPT_INCLUDED', true);
 		}
 	} 
