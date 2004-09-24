9dcf1ce9d7f8ad2036189dda2c0e65484f1f521d
moltyaninov <moltyaninov@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-09-24 12:03:48 +0000

    -- location bar emulator added for control panel iframes;

--- php4/branches/2.2.3/shared/js/store_tab_url.js
+++ php4/branches/2.2.3/shared/js/store_tab_url.js
@@ -3,6 +3,14 @@
 		wname = window.name;
 		tab_name = 'tab_' + wname.substr(7);
 		set_cookie(tab_name + '_last_url', escape(window.location.href));
+		set_cookie('active_tab',wname.substr(7));
 	}
 	
+	function refresh_location_bar()
+	{
+		if(typeof(top.refresh_url) == 'function')
+		  top.refresh_url();
+	}
+  
 	add_event(window, 'unload', store_tab_url)
+	add_event(window, 'load', refresh_location_bar)
