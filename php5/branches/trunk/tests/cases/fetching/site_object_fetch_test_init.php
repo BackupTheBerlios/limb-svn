92498a5dd5d0efcd0f28b820b944b570e64d8912
pachanga <pachanga@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-10-01 10:54:58 +0000

    -- tests repository layout changes

--- php5/branches/trunk/tests/cases/fetching/site_object_fetch_test_init.php
+++ php5/branches/trunk/tests/cases/fetching/site_object_fetch_test_init.php
@@ -7,12 +7,11 @@
 *
 * $Id$
 *
-***********************************************************************************/ 
-
+***********************************************************************************/
 require_once(LIMB_DIR . 'class/lib/db/db_factory.class.php');
 
 class site_object_fetch_test_version extends site_object
-{		
+{
 	function _define_attributes_definition()
 	{
 		return complex_array :: array_merge(
@@ -21,9 +20,9 @@ class site_object_fetch_test_version extends site_object
 				'title' => '',
 				'name' => array('type' => 'numeric'),
 				'search' => array('search' => true),
-				));		
+				));
 	}
-	
+
 	function _define_class_properties()
 	{
 		return array(
@@ -36,12 +35,12 @@ class site_object_fetch_test_version extends site_object
 }
 
 class site_object_fetch_test_init
-{ 
+{
 	var $db = null;
 	var $class_id = '';
 	var $root_node_id = '';
-	
-  function site_object_fetch_test_init() 
+
+  function site_object_fetch_test_init()
   {
 		$this->db = db_factory :: instance();
 
@@ -51,7 +50,7 @@ class site_object_fetch_test_init
   function init(& $object)
   {
   	$this->class_id = $object->get_class_id();
-  	
+
   	$this->_insert_sys_site_object_records();
   	$this->_insert_fake_sys_site_object_records();
   }
@@ -59,7 +58,7 @@ class site_object_fetch_test_init
   function _insert_sys_site_object_records()
   {
   	$db_table =& db_table_factory :: create('sys_site_object');
-  	
+
   	$tree =& tree :: instance();
 		$values['identifier'] = 'root';
 		$this->root_node_id = $tree->create_root_node($values, false, true);
@@ -69,8 +68,8 @@ class site_object_fetch_test_init
   	{
   		$version = mt_rand(1, 3);
   		$this->_insert_object_version_records($i, $version);
-  		
-  		$this->db->sql_insert('sys_site_object', 
+
+  		$this->db->sql_insert('sys_site_object',
   			array(
   				'id' => $i,
   				'class_id' => $this->class_id,
@@ -91,20 +90,20 @@ class site_object_fetch_test_init
   function _insert_object_version_records($object_id, $version_max)
   {
 	}
-	
+
   function _insert_fake_sys_site_object_records()
   {
   	$class_db_table = db_table_factory :: create('sys_class');
   	$class_db_table->insert(array('id' => 1001, 'class_name' => 'fake_class'));
-  	
+
   	$db_table =& db_table_factory :: create('sys_site_object');
 
   	$tree =& tree :: instance();
-  	
+
   	$data = array();
   	for($i = 11; $i <= 20 ; $i++)
   	{
-  		$this->db->sql_insert('sys_site_object', 
+  		$this->db->sql_insert('sys_site_object',
   			array(
   				'id' => $i,
   				'class_id' => 1001,
@@ -114,13 +113,13 @@ class site_object_fetch_test_init
   				'locale_id' => 'en',
   			)
   		);
-  	
+
 			$values['identifier'] = 'object_' . $i;
 			$values['object_id'] = $i;
 			$tree->create_sub_node($this->root_node_id, $values);
   	}
   }
-  
+
   function _clean_up()
   {
   	$this->db->sql_delete('sys_site_object');
