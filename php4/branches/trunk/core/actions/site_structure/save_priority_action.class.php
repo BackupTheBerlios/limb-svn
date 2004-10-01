97bad6cd44871abf375d964caa2f4de92907299c
moltyaninov <moltyaninov@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-10-01 10:50:29 +0000

    -- bug_fix: save_object_priority moved to site_object class form deleted site_structure; -- some template changes;

--- php4/branches/trunk/core/actions/site_structure/save_priority_action.class.php
+++ php4/branches/trunk/core/actions/site_structure/save_priority_action.class.php
@@ -12,24 +12,24 @@ require_once(LIMB_DIR . 'core/actions/form_action.class.php');
 
 class save_priority_action extends form_action
 {
-	function _define_dataspace_name()
-	{
-	  return 'grid_form';
-	}
-	
-	function _valid_perform(&$request, &$response)
-	{
-		$data = $this->dataspace->export();
-		$object =& site_object_factory :: create('site_structure');
-		
-		if(isset($data['priority']))
-			$object->save_priority($data['priority']);
-    
+  function _define_dataspace_name()
+  {
+    return 'grid_form';
+  }
+
+  function _valid_perform(&$request, &$response)
+  {
+    $data = $this->dataspace->export();
+    $object =& site_object_factory :: create('site_object');
+
+    if(isset($data['priority']))
+      $object->save_priority($data['priority']);
+
     $request->set_status(REQUEST_STATUS_SUCCESS);
-    
-		if($request->has_attribute('popup'))
-		  $response->write(close_popup_response($request));
-	}
+
+    if($request->has_attribute('popup'))
+      $response->write(close_popup_response($request));
+  }
 }
 
 ?>
