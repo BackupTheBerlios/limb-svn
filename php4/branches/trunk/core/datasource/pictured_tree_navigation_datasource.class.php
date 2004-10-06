cb2b1c74c04b4a2ebae03f25284cb85243df55c1
moltyaninov <moltyaninov@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-10-06 14:20:49 +0000

    -- pictured navigation datasource added (used for administration side); -- some html templates refactored;

---
+++ php4/branches/trunk/core/datasource/pictured_tree_navigation_datasource.class.php
@@ -0,0 +1,52 @@
+<?php
+/**********************************************************************************
+* Copyright 2004 BIT, Ltd. http://www.0x00.ru, mailto: bit@0x00.ru
+*
+* Released under the LGPL license (http://www.gnu.org/copyleft/lesser.html)
+***********************************************************************************
+*
+* $Id$
+*
+***********************************************************************************/
+require_once(LIMB_DIR . 'core/datasource/tree_navigation_datasource.class.php');
+
+class pictured_tree_navigation_datasource extends tree_navigation_datasource
+{
+  function & _fetch(&$counter, $params)
+  {
+    $tree =& parent :: _fetch($counter, $params);
+    foreach($tree as $key => $data)
+    {
+      if(file_exists(PROJECT_DIR. 'design/main/images/menu_icons/'. $data['identifier'] .'.32.gif'))
+      {
+        $tree[$key]['image_path'] = '/design/main/images/menu_icons/';
+        $tree[$key]['image_name'] = $data['identifier'];
+      }
+      else if(file_exists(SHARED_DIR. 'images/menu_icons/'. $data['identifier'] .'.32.gif'))
+      {
+        $tree[$key]['image_path'] = SHARED_IMG_URL . 'menu_icons/';
+        $tree[$key]['image_name'] = $data['identifier'];
+      }
+      else
+      {
+        $tree[$key]['image_path'] = SHARED_IMG_URL .'menu_icons/';
+        $tree[$key]['image_name'] = 'default';
+      }
+
+      if($data['level'] == 1)
+      {
+        $result[$key] = $tree[$key];
+        $current_key = $key;
+      }
+      if($data['level'] > 1)
+      {
+        $result[$current_key]['child_items'][$key] = $tree[$key];
+      }
+    }
+    return $result;
+  }
+
+}
+
+
+?>
