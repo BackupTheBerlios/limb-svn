5f5a5be4cbb6a7b06ab569fc0683cb83a5f6c72e
moltyaninov <moltyaninov@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-10-19 09:05:01 +0000

    -- smal locale bug fix (get_month, month numeration begins at 0);

--- php4/branches/2.2.2/core/template/components/locale_date_format_component.class.php
+++ php4/branches/2.2.2/core/template/components/locale_date_format_component.class.php
@@ -17,23 +17,23 @@ require_once(LIMB_DIR . 'core/lib/i18n/locale.class.php');
 class locale_date_format_component extends component
 {
 	var $date = null;
-	
+
 	var $date_type = 'string';
-		
+
 	var $format_string = '';
-	
+
 	var $locale_type = CONTENT_LOCALE_ID;
 
 	function prepare()
 	{
 		$this->date =& new date();
 	}
-	
+
 	function set_format_string($string)
 	{
 		$this->format_string = $string;
 	}
-	
+
 	function set_date_type($type)
 	{
 		$this->date_type = $type;
@@ -46,25 +46,25 @@ class locale_date_format_component extends component
 		else
 			$this->locale_type = CONTENT_LOCALE_ID;
 	}
-	
+
 	function set_locale_format_type($type)
-	{		
+	{
 		$locale =& locale :: instance($this->locale_type);
 
 		switch($type)
-		{			
+		{
 			case 'time':
 				$this->format_string = $locale->get_time_format();
 			break;
-			
+
 			case 'short_time':
 				$this->format_string = $locale->get_short_time_format();
 			break;
-			
+
 			case 'date':
 				$this->format_string = $locale->get_date_format();
 			break;
-			
+
 			case 'short_date':
 				$this->format_string = $locale->get_short_date_format();
 			break;
@@ -76,12 +76,12 @@ class locale_date_format_component extends component
 			case 'short_date_time':
 				$this->format_string = $locale->get_short_date_time_format();
 			break;
-			
+
 			default:
 				$this->format_string = $locale->get_short_date_format();
 		}
 	}
-	
+
 	function set_date($date_string)
 	{
 		switch($this->date_type)
@@ -89,13 +89,13 @@ class locale_date_format_component extends component
 			case 'string':
 				$this->date->set_by_string($date_string);
 			break;
-			
+
 			case 'stamp':
 				$this->date->set_by_stamp((int)$date_string);
 			break;
 		}
 	}
-	
+
 	function format()
 	{
 		if($this->format_string)
@@ -104,11 +104,11 @@ class locale_date_format_component extends component
 		{
 			$locale =& locale :: instance($this->locale_type);
 			$format_string = $locale->get_short_date_format();
-		}	
-		
+		}
+
 		echo $this->date->format($format_string);
 	}
-	
-} 
+
+}
 
 ?>
