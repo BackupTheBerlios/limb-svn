b4cc54c34e598faecad4e19ffab960da9a00a286
moltyaninov <moltyaninov@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-10-21 10:59:12 +0000

    -- grid_actions bug fixed (javascript submit_grid_form moved from tag to /sahred/js/common.js, otherwise there was a javascript bug in case of two grid lists on one page);

--- php4/branches/trunk/shared/js/common.js
+++ php4/branches/trunk/shared/js/common.js
@@ -158,7 +158,7 @@ function toggle_obj_display(obj)
 function _optimize_window()
 {
 	var body = document.body;
-	
+
 	if (is_gecko)
     window.sizeToContent();
 
@@ -488,6 +488,14 @@ function submit_form(form, form_action)
   form.submit();
 }
 
+function submit_grid_form(button, selector_id)
+{
+  menu = document.getElementById(selector_id);
+  action = menu.options[menu.selectedIndex].value;
+  if(action != '')
+    submit_form(button.form, action);
+}
+
 function process_action_control(droplist)
 {
   if (typeof(droplist.value) != 'undefined')
@@ -788,29 +796,29 @@ function var_dump(obj, level)
 }
 
 function scramble(str, offset)
-{ 
+{
   if (!offset) offset = 1;
-  
-  var r = ''; 
-  for(var i=0;i<str.split('').length;i++) 
-    r += String.fromCharCode(str.split('')[i].charCodeAt(0) + offset); 
+
+  var r = '';
+  for(var i=0;i<str.split('').length;i++)
+    r += String.fromCharCode(str.split('')[i].charCodeAt(0) + offset);
   return r
 }
 
 function get_msn_status_icon(email, icon_set)
 {
   if (!icon_set) icon_set = 3;
-  
+
   st = "<img alt='" + email + "' title='" + email + "' onerror='this.onerror=null;this.src=\"http://www.the-server.net/osi" + icon_set + "/image/msnunknown.gif\";' src='http://www.the-server.net/osi" + icon_set + "/msn/" + email + "' align='absMiddle' border='0' />"
-  
+
   return st;
 }
 
 function get_icq_status_icon(icq_number, icon_set)
 {
   if (!icon_set) icon_set = 5;
-  
+
   st = "<img alt='" + icq_number + "' title='" + icq_number + "' src='http://online.mirabilis.com/scripts/online.dll?icq=" + icq_number + "&img=" + icon_set + "' align='absMiddle' border='0' />"
-  
+
   return st;
 }
