bbfba2bf1e3d5f7b5c238234971a19898ec437da
seregalimb <seregalimb@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-10-25 08:00:21 +0000

    -- articles package articles rewrite due to commands implementation

---
+++ packages/php5/articles/branches/trunk/commands/article/create_article_command.class.php
@@ -0,0 +1,21 @@
+<?php
+/**********************************************************************************
+* Copyright 2004 BIT, Ltd. http://limb-project.com, mailto: limb@0x00.ru
+*
+* Released under the LGPL license (http://www.gnu.org/copyleft/lesser.html)
+***********************************************************************************
+*
+* $Id: create_article_action.class.php 786 2004-10-12 14:24:43Z pachanga $
+*
+***********************************************************************************/
+require_once(LIMB_DIR . '/class/core/commands/create_site_object_command.class.php');
+
+class create_article_command extends create_site_object_command
+{
+	protected function _define_site_object_class_name()
+	{
+	  return 'article';
+	}  
+}
+
+?>
---
+++ packages/php5/articles/branches/trunk/commands/article/edit_article_command.class.php
@@ -0,0 +1,21 @@
+<?php
+/**********************************************************************************
+* Copyright 2004 BIT, Ltd. http://limb-project.com, mailto: limb@0x00.ru
+*
+* Released under the LGPL license (http://www.gnu.org/copyleft/lesser.html)
+***********************************************************************************
+*
+* $Id: edit_article_action.class.php 786 2004-10-12 14:24:43Z pachanga $
+*
+***********************************************************************************/
+require_once(LIMB_DIR . '/class/core/commands/edit_site_object_command.class.php');
+
+class edit_article_command extends edit_site_object_command
+{
+	protected function _define_site_object_class_name()
+	{
+	  return 'article';
+	}  
+}
+
+?>
