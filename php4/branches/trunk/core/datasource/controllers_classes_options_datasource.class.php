558327425b778c36e3d824db863b56e7026601b5
pachanga <pachanga@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-11-09 15:33:28 +0000

    -- minor bug fix

--- php4/branches/trunk/core/datasource/controllers_classes_options_datasource.class.php
+++ php4/branches/trunk/core/datasource/controllers_classes_options_datasource.class.php
@@ -7,30 +7,33 @@
 *
 * $Id$
 *
-***********************************************************************************/ 
+***********************************************************************************/
 require_once(LIMB_DIR . '/core/datasource/datasource.class.php');
 require_once(LIMB_DIR . '/core/lib/system/fs.class.php');
 
 class controllers_classes_options_datasource extends datasource
 {
-	function get_default_option()
-	{
-		return 0;
-	}
-	
-	function get_options_array()
-	{
-		$result = array(0 => '----');
-    
+  function get_default_option()
+  {
+    return 0;
+  }
+
+  function get_options_array()
+  {
+    $result = array();
+
     $this->_add_project_controllers($result);
-    
-    $result[-1] = '----';//???
-    
+
+    if($result)
+      array_unshift($result, '---');
+
+    $result[-1] = '---';
+
     $this->_add_limb_controllers($result);
-    
-		return $result;
-	}
-  
+
+    return $result;
+  }
+
   function _add_limb_controllers(&$result)
   {
     $items = fs :: find_subitems(LIMB_DIR . '/core/controllers/', 'f', '~site_object_controller~', false);
@@ -39,20 +42,20 @@ class controllers_classes_options_datasource extends datasource
     {
       $class = $this->_clean_class_path($item);
       $result[$class] = $class;
-    }    
+    }
   }
-  
+
   function _add_project_controllers(&$result)
   {
     $items = fs :: find_subitems(PROJECT_DIR . '/core/controllers/', 'f', '', false);
-    
+
     foreach($items as $item)
     {
       $class = $this->_clean_class_path($item);
       $result[$class] = $class;
     }
   }
-  
+
   function _clean_class_path($class_path)
   {
     preg_match('~^([^\.]*)\.class\.php$~', $class_path, $matches);
--- php4/branches/trunk/core/datasource/site_objects_classes_options_datasource.class.php
+++ php4/branches/trunk/core/datasource/site_objects_classes_options_datasource.class.php
@@ -7,30 +7,33 @@
 *
 * $Id$
 *
-***********************************************************************************/ 
+***********************************************************************************/
 require_once(LIMB_DIR . '/core/datasource/datasource.class.php');
 require_once(LIMB_DIR . '/core/lib/system/fs.class.php');
 
 class site_objects_classes_options_datasource extends datasource
 {
-	function get_default_option()
-	{
-		return 0;
-	}
-	
-	function get_options_array()
-	{
-		$result = array(0 => '----');
-    
+  function get_default_option()
+  {
+    return 0;
+  }
+
+  function get_options_array()
+  {
+    $result = array();
+
     $this->_add_project_site_objects($result);
-    
-    $result[-1] = '----';//???
-    
+
+    if($result)
+      array_unshift($result, '---');
+
+    $result[-1] = '---';
+
     $this->_add_limb_site_objects($result);
-    
-		return $result;
-	}
-  
+
+    return $result;
+  }
+
   function _add_limb_site_objects(&$result)
   {
     //media object can't really be a valid site object(refactor!!!)
@@ -40,20 +43,20 @@ class site_objects_classes_options_datasource extends datasource
     {
       $class = $this->_clean_class_path($item);
       $result[$class] = $class;
-    }    
+    }
   }
-  
+
   function _add_project_site_objects(&$result)
   {
     $items = fs :: find_subitems(PROJECT_DIR . '/core/model/site_objects/', 'f', '', false);
-    
+
     foreach($items as $item)
     {
       $class = $this->_clean_class_path($item);
       $result[$class] = $class;
     }
   }
-  
+
   function _clean_class_path($class_path)
   {
     preg_match('~^([^\.]*)\.class\.php$~', $class_path, $matches);
