d839148da9e164892201a6600c5b578ad387e1c3
moltyaninov <moltyaninov@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-11-12 13:46:43 +0000

    -- cart_item & cart refactored to simplify inheritance;

--- php4/branches/2.3alpha/core/actions/cart/add_cart_items_action.class.php
+++ php4/branches/2.3alpha/core/actions/cart/add_cart_items_action.class.php
@@ -7,77 +7,77 @@
 *
 * $Id$
 *
-***********************************************************************************/ 
+***********************************************************************************/
 require_once(LIMB_DIR . '/core/actions/form_action.class.php');
 require_once(LIMB_DIR . '/core/model/shop/cart.class.php');
 
 class add_cart_items_action extends form_action
 {
-	function _define_dataspace_name()
-	{
-	  return 'order_form';
-	}
-	
-	function _define_catalog_object()
-	{
-	  return 'catalog_object';
-	}
-	
-	function _valid_perform(&$request, &$response)
-	{
-		if(!$objects_amounts = $this->dataspace->get('amount'))
-		{
-			$request->set_status(REQUEST_STATUS_FAILURE);
-			
-  		if($request->has_attribute('popup'))
-  			$response->write(close_popup_response($request));
-  			
-  		return;			
-		}
-			
-		$objects_data =& fetch_by_node_ids(
-													array_keys($objects_amounts), 
-													$this->_define_catalog_object(),
-													$counter);
-	
-		if(!$objects_data)
-		{
-			$request->set_status(REQUEST_STATUS_FAILURE);
-			
-  		if($request->has_attribute('popup'))
-  			$response->write(close_popup_response($request));
-  			
-  		return;
-		}
-			
-		$object =& site_object_factory :: create($this->_define_catalog_object());
+  function _define_dataspace_name()
+  {
+    return 'order_form';
+  }
 
-		if(!method_exists($object, 'get_cart_item'))
-		{
-			$request->set_status(REQUEST_STATUS_FAILURE);
-			
-  		if($request->has_attribute('popup'))
-  			$response->write(close_popup_response($request));
-  			
-  		return;
-		}
+  function _define_catalog_object()
+  {
+    return 'catalog_object';
+  }
 
-		$cart =& cart :: instance();
-		
-		foreach($objects_data as $key => $object_data)
-		{
-			$object->import_attributes($object_data);
-			$cart_item =& $object->get_cart_item();
-			
-			if(!$objects_amounts[$key])
-				continue;
-				
-			$cart_item->set_amount($objects_amounts[$key]);
-			$cart->add_item($cart_item);
-		}
-		
-		$response->redirect('/root/cart?popup=1');
-	}
+  function _valid_perform(&$request, &$response)
+  {
+    if(!$objects_amounts = $this->dataspace->get('amount'))
+    {
+      $request->set_status(REQUEST_STATUS_FAILURE);
+
+      if($request->has_attribute('popup'))
+        $response->write(close_popup_response($request));
+
+      return;
+    }
+
+    $objects_data =& fetch_by_node_ids(
+                          array_keys($objects_amounts),
+                          $this->_define_catalog_object(),
+                          $counter);
+
+    if(!$objects_data)
+    {
+      $request->set_status(REQUEST_STATUS_FAILURE);
+
+      if($request->has_attribute('popup'))
+        $response->write(close_popup_response($request));
+
+      return;
+    }
+
+    $object =& site_object_factory :: create($this->_define_catalog_object());
+
+    if(!method_exists($object, 'get_cart_item'))
+    {
+      $request->set_status(REQUEST_STATUS_FAILURE);
+
+      if($request->has_attribute('popup'))
+        $response->write(close_popup_response($request));
+
+      return;
+    }
+
+    $cart =& cart :: instance();
+
+    foreach($objects_data as $key => $object_data)
+    {
+      $object->import_attributes($object_data);
+      $cart_item =& $object->get_cart_item();
+
+      if(!$objects_amounts[$key])
+        continue;
+
+      $cart_item->set_amount($objects_amounts[$key]);
+      $cart->add_item($cart_item);
+    }
+
+    $response->redirect('/root/cart?popup=1');
+  }
 }
 
 ?>
