62dc63cef106e896aee28ddaf1f1378b7f28be8d
seregalimb <seregalimb@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2004-12-10 12:18:13 +0000

    -- minor bug in create_documents_folder_action. Method should can _define_controller_name().

--- php4/branches/trunk/core/actions/documents_folder/create_documents_folder_action.class.php
+++ php4/branches/trunk/core/actions/documents_folder/create_documents_folder_action.class.php
@@ -12,7 +12,7 @@ require_once(LIMB_DIR . '/core/actions/site_object/create_action.class.php');
 
 class create_documents_folder_action extends create_action
 {
-  function _define_dataspace_name()
+  function _define_controller_name()
   {
     return 'documents_folder_controller';
   }
