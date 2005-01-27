b7f1ecb112fcb6dc1dcc687a4cb410606c4863d7
pachanga <pachanga@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2005-01-27 14:53:05 +0000

    -- ComplexSelectSQL class + tests added

---
+++ php4/branches/2.4/core/db/ComplexSelectSQL.class.php
@@ -0,0 +1,169 @@
+<?php
+/**********************************************************************************
+* Copyright 2004 BIT, Ltd. http://limb-project.com, mailto: support@limb-project.com
+*
+* Released under the LGPL license (http://www.gnu.org/copyleft/lesser.html)
+***********************************************************************************
+*
+* $Id$
+*
+***********************************************************************************/
+
+class ComplexSelectSQL
+{
+  var $_raw_sql;
+  var $_joins;
+  var $_fields;
+  var $_constraints;
+  var $_join_constraints;
+  var $_order;
+  var $_group_by;
+
+  function ComplexSelectSQL($raw_sql)
+  {
+    $this->_raw_sql = $raw_sql;
+    $this->reset();
+  }
+
+  function reset()
+  {
+    $this->_joins = array();
+    $this->_fields = array();
+    $this->_constraints = array();
+    $this->_join_constraints = array();
+    $this->_order = array();
+    $this->_group_by = array();
+  }
+
+  function addField($field)
+  {
+    $this->_fields[] = $field;
+  }
+
+  function addOrder($field, $type='ASC')
+  {
+    $this->_order[] = "$field $type";
+  }
+
+  function addGroupBy($group)
+  {
+    $this->_group_by[] = $group;
+  }
+
+  function addJoin($table, $connect_by)
+  {
+    $this->_join_constraints[$table] = $connect_by;
+  }
+
+  function addCondition($condition)
+  {
+    $this->_constraints[] = $condition;
+  }
+
+  function toString()
+  {
+    $replace = array('%fields%' => $this->_createFieldsClause(),
+                     '%join%' => $this->_createJoinClause(),
+                     '%where%' => $this->_createWhereClause(),
+                     '%order%' => $this->_createOrderClause(),
+                     '%group_by%' => $this->_createGroupClause());
+
+    return trim(strtr($this->_raw_sql, $replace));
+  }
+
+  function _getCleanSQL()
+  {
+    $replace = array('%fields%' => '',
+                     '%join%' => '',
+                     '%where%' => '',
+                     '%order%' => '',
+                     '%group_by%' => '');
+
+    return strtr($this->_raw_sql, $replace);
+  }
+
+  function _createFieldsClause()
+  {
+    if (count($this->_fields) == 0)
+      return '*';
+
+    return implode(',', $this->_fields);
+  }
+
+  function _createJoinClause()
+  {
+    $join = array();
+    foreach ($this->_join_constraints as $table => $connect_by)
+    {
+      $foreign_key = key($connect_by);
+      $alias_key = reset($connect_by);
+      $join[] = "LEFT JOIN $table ON $foreign_key=$alias_key";
+    }
+
+    return implode(' ', $join);
+  }
+
+  function _createWhereClause()
+  {
+    if (count($this->_constraints) == 0)
+      return '';
+
+    $where = '(' . implode(') AND (', $this->_constraints) . ')';
+
+    $sql = $this->_getCleanSQL();
+
+    //primitive check if WHERE was already in sql
+    //!!!make it better later
+    if(preg_match('~(?<=from).+where\s~i', $sql))
+      return $where;
+    else
+      return 'WHERE ' . $where;
+  }
+
+  function _createGroupClause()
+  {
+    if (count($this->_group_by) == 0)
+      return '';
+
+    $group = implode(',', $this->_group_by);
+
+    $sql = $this->_getCleanSQL();
+
+    //primitive check if GROUP BY was already in sql
+    //!!!make it better later
+    if(preg_match('~(?<=from).+group\s+by\s(.*)$~i', $sql, $matches))
+    {
+      //primitive check if comma is required
+      if(trim($matches[1]) != '')
+        return ',' . $group;
+      else
+        return $group;
+    }
+    else
+      return 'GROUP BY ' . $group;
+  }
+
+  function _createOrderClause()
+  {
+    if (count($this->_order) == 0)
+      return '';
+
+    $order = implode(',', $this->_order);
+
+    $sql = $this->_getCleanSQL();
+
+    //primitive check if ORDER BY was already in sql
+    //!!!make it better later
+    if(preg_match('~(?<=from).+order\s+by\s(.*)$~i', $sql, $matches))
+    {
+      //primitive check if comma is required
+      if(trim($matches[1]) != '')
+        return ',' . $order;
+      else
+        return $order;
+    }
+    else
+      return 'ORDER BY ' . $order;
+  }
+}
+?>
---
+++ php4/branches/2.4/tests/cases/db/ComplexSelectSQLTest.class.php
@@ -0,0 +1,202 @@
+<?php
+/**********************************************************************************
+* Copyright 2004 BIT, Ltd. http://limb-project.com, mailto: support@limb-project.com
+*
+* Released under the LGPL license (http://www.gnu.org/copyleft/lesser.html)
+***********************************************************************************
+*
+* $Id$
+*
+***********************************************************************************/
+require_once(LIMB_DIR . '/core/db/ComplexSelectSQL.class.php');
+
+class ComplexSelectSQLTest extends LimbTestCase
+{
+  function ComplexSelectSQLTest()
+  {
+    parent :: LimbTestCase('complex select sql tests');
+  }
+
+  function testSelect()
+  {
+    $sql = new ComplexSelectSQL('SELECT * FROM test');
+
+    $this->assertEqual($sql->toString(), 'SELECT * FROM test');
+  }
+
+  function testNoFields()
+  {
+    $sql = new ComplexSelectSQL('SELECT %fields% FROM test');
+
+    $this->assertEqual($sql->toString(), 'SELECT * FROM test');
+  }
+
+  function testAddField()
+  {
+    $sql = new ComplexSelectSQL('SELECT %fields% FROM test');
+
+    $sql->addField('t1');
+    $sql->addField('t2');
+
+    $this->assertEqual($sql->toString(), 'SELECT t1,t2 FROM test');
+  }
+
+  function testNoJoin()
+  {
+    $sql = new ComplexSelectSQL('SELECT * FROM test %join%');
+
+    $expected = 'SELECT * FROM test';
+
+    $this->assertEqual($sql->toString(), $expected);
+  }
+
+  function testAddJoin()
+  {
+    $sql = new ComplexSelectSQL('SELECT * FROM test %join%');
+
+    $sql->addJoin('article', array('test.article_id' => 'article.id'));
+
+    $expected = 'SELECT * FROM test LEFT JOIN article ON test.article_id=article.id';
+
+    $this->assertEqual($sql->toString(), $expected);
+  }
+
+  function testEmptyCondition()
+  {
+    $sql = new ComplexSelectSQL('SELECT * FROM test %where%');
+
+    $this->assertEqual($sql->toString(),
+                       'SELECT * FROM test');
+  }
+
+  function testAddCondition()
+  {
+    $sql = new ComplexSelectSQL('SELECT * FROM test WHERE %where%');
+
+    $sql->addCondition('c1=:c1 OR c2=:c2');
+
+    $this->assertEqual($sql->toString(),
+                       'SELECT * FROM test WHERE (c1=:c1 OR c2=:c2)');
+  }
+
+  function testAddConditionNoWhereClause()
+  {
+    $sql = new ComplexSelectSQL('SELECT * FROM test %where%');
+
+    $sql->addCondition('c1=:c1 OR c2=:c2');
+
+    $this->assertEqual($sql->toString(),
+                       'SELECT * FROM test WHERE (c1=:c1 OR c2=:c2)');
+  }
+
+  function testAddSeveralConditions()
+  {
+    $sql = new ComplexSelectSQL('SELECT * FROM test %where%');
+
+    $sql->addCondition('c1=:c1');
+    $sql->addCondition('c2=:c2');
+
+    $this->assertEqual($sql->toString(),
+                       'SELECT * FROM test WHERE (c1=:c1) AND (c2=:c2)');
+  }
+
+  function testEmptyOrder()
+  {
+    $sql = new ComplexSelectSQL('SELECT * FROM test %order%');
+
+    $this->assertEqual($sql->toString(),
+                       'SELECT * FROM test');
+  }
+
+  function testAddOrderNoOrderClause()
+  {
+    $sql = new ComplexSelectSQL('SELECT * FROM test %order%');
+
+    $sql->addOrder('t1');
+    $sql->addOrder('t2', 'DESC');
+
+    $this->assertEqual($sql->toString(),
+                       'SELECT * FROM test ORDER BY t1 ASC,t2 DESC');
+  }
+
+  function testAddOrderWithOrderClause()
+  {
+    $sql = new ComplexSelectSQL('SELECT * FROM test ORDER BY %order%');
+
+    $sql->addOrder('t1');
+    $sql->addOrder('t2', 'DESC');
+
+    $this->assertEqual($sql->toString(),
+                       'SELECT * FROM test ORDER BY t1 ASC,t2 DESC');
+  }
+
+  function testAddOrderWithOrderClause2()
+  {
+    $sql = new ComplexSelectSQL('SELECT * FROM test ORDER BY t0 DESC %order%');
+
+    $sql->addOrder('t1');
+    $sql->addOrder('t2', 'DESC');
+
+    $this->assertEqual($sql->toString(),
+                       'SELECT * FROM test ORDER BY t0 DESC ,t1 ASC,t2 DESC');
+  }
+
+  function testAddOrderWithOrderClause3()
+  {
+    $sql = new ComplexSelectSQL('SELECT * FROM test ORDER BY t0 DESC %order%');
+
+    $this->assertEqual($sql->toString(),
+                       'SELECT * FROM test ORDER BY t0 DESC');
+  }
+
+  function testEmptyGroupBy()
+  {
+    $sql = new ComplexSelectSQL('SELECT * FROM test');
+
+    $this->assertEqual($sql->toString(),
+                       'SELECT * FROM test');
+  }
+
+  function testAddGroupBy()
+  {
+    $sql = new ComplexSelectSQL('SELECT * FROM test %group_by%');
+
+    $sql->addGroupBy('t1');
+    $sql->addGroupBy('t2');
+
+    $this->assertEqual($sql->toString(),
+                       'SELECT * FROM test GROUP BY t1,t2');
+  }
+
+  function testAddGroupByWithGroupByClause()
+  {
+    $sql = new ComplexSelectSQL('SELECT * FROM test GROUP BY %group_by%');
+
+    $sql->addGroupBy('t1');
+    $sql->addGroupBy('t2');
+
+    $this->assertEqual($sql->toString(),
+                       'SELECT * FROM test GROUP BY t1,t2');
+  }
+
+  function testAddGroupByWithGroupByClause2()
+  {
+    $sql = new ComplexSelectSQL('SELECT * FROM test GROUP BY t0 %group_by%');
+
+    $sql->addGroupBy('t1');
+    $sql->addGroupBy('t2');
+
+    $this->assertEqual($sql->toString(),
+                       'SELECT * FROM test GROUP BY t0 ,t1,t2');
+  }
+
+  function testAddGroupByWithGroupByClause3()
+  {
+    $sql = new ComplexSelectSQL('SELECT * FROM test GROUP BY t0 %group_by%');
+
+    $this->assertEqual($sql->toString(),
+                       'SELECT * FROM test GROUP BY t0');
+  }
+
+}
+?>
