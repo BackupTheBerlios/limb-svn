44f5a78a253ab41305cb204c59a605b1ecd80800
moltyaninov <moltyaninov@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2005-03-16 15:13:27 +0000

    -- added method get_mailer (mail.inc.php), which creates mailer and customize it; -- some FakemailDaemon improvements;

--- php4/branches/trunk/tests/lib/fakemail_daemon.class.php
+++ php4/branches/trunk/tests/lib/fakemail_daemon.class.php
@@ -8,23 +8,39 @@
 * $Id$
 *
 ***********************************************************************************/
+@define('FAKEMAIL_PORT', 25);
+@define('FAKEMAIL_HOST', 'localhost');
+@define('FAKEMAIL_PATH', '.');
+@define('FAKEMAIL_SCRIPT', './fakemail');
 
 class FakemailDaemon
 {
   var $pid = null;
   var $path = null;
   var $port = null;
-  var $path = null;
+  var $host = null;
   var $log  = null;
+
+  function FakemailDaemon($path = null, $port = null, $host = null)
+  {
+    $this->path = is_null($path) ?  FAKEMAIL_PATH : $path;
+    $this->port = is_null($port) ?  FAKEMAIL_PORT : $port;
+    $this->host = is_null($host) ?  FAKEMAIL_HOST : $host;
+  }
+
+  function useLog($log = null)
+  {
+    $this->log = is_null($log) ?  $this->path . '/fakemail.log' : $log;
+  }
 
   function start()
   {
-    $this->path = FAKEMAIL_PATH;
-    $this->port = FAKEMAIL_PORT;
-    $this->host = FAKEMAIL_HOST;
-    $this->log  = FAKEMAIL_PATH . '/fakemail.log';
+    if(!file_exists($this->path) || !is_dir($this->path))
+      die('Directory for fakemail \"'. $this->path .'\" not found' );
 
-    $fakemail = "perl ". FAKEMAIL_SCRIPT ." --background --path={$this->path} --port={$this->port} --host={$this->host} --log={$this->log}" ;
+    $fakemail = "perl ". FAKEMAIL_SCRIPT ." --background --path={$this->path} --port={$this->port} --host={$this->host}" ;
+    if($this->log)
+      $fakemail .= " --log={$this->log}";
     $this->pid = exec($fakemail);
   }
 
@@ -41,26 +57,26 @@ class FakemailDaemon
 
   function removeRecipientMail($recipient)
   {
-     $names = $this->getRecipientFileNames($recipient);
+     $names = $this->_getRecipientFileNames($recipient);
      foreach($names as $name)
        $contents[] = unlink($this->path .'/'. $name);
   }
 
   function getRecipientMailCount($recipient)
   {
-    return count($this->getRecipientFileNames($recipient));
+    return count($this->_getRecipientFileNames($recipient));
   }
 
   function getRecipientMailContents($recipient)
   {
      $contents = array();
-     $names = $this->getRecipientFileNames($recipient);
+     $names = $this->_getRecipientFileNames($recipient);
      foreach($names as $name)
        $contents[] = file_get_contents($this->path .'/'. $name);
      return $contents;
   }
 
-  function getRecipientFileNames($recipient)
+  function _getRecipientFileNames($recipient)
   {
     $this_path = getcwd();
     $recipient_files = array();
