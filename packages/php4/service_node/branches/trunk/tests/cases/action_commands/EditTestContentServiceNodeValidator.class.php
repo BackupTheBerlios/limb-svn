17a414a21e413cd29832bafb59373d50f8d10381
seregalimb <seregalimb@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2005-05-05 11:36:02 +0000

    -- temp commit

---
+++ packages/php4/service_node/branches/trunk/tests/cases/action_commands/EditTestContentServiceNodeValidator.class.php
@@ -0,0 +1,24 @@
+<?php
+/**********************************************************************************
+* Copyright 2004 BIT, Ltd. http://limb-project.com, mailto: support@limb-project.com
+*
+* Released under the LGPL license (http://www.gnu.org/copyleft/lesser.html)
+***********************************************************************************
+*
+* $Id: OneTableObjectMapper.class.php 1094 2005-02-08 13:09:14Z pachanga $
+*
+***********************************************************************************/
+require_once(LIMB_SERVICE_NODE_DIR . '/validators/CommonEditServiceNodeValidator.class.php');
+
+class EditTestContentServiceNodeValidator extends CommonEditServiceNodeValidator
+{
+  function validate(&$DataSource)
+  {
+    $this->addRule(new Handle(WACT_ROOT . '/validation/rule.inc.php|RequiredRule', array('annotation')));
+    $this->addRule(new Handle(WACT_ROOT . '/validation/rule.inc.php|RequiredRule', array('content')));
+
+    parent :: validate($DataSource);
+ }
+}
+
+?>
