bb77c4700448b8c8171b7dbec7e76d088b11ed87
pachanga <pachanga@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2005-05-06 10:49:55 +0000

    -- ip tag php includes

--- php4/branches/trunk/core/template/tags/core/ip.tag.php
+++ php4/branches/trunk/core/template/tags/core/ip.tag.php
@@ -23,11 +23,15 @@ class ip_tag extends compiler_directive_tag
   {
     if(isset($this->attributes['hash_id']))
     {
+      $code->register_include(LIMB_DIR . '/core/lib/http/ip.class.php');
       $code->write_php(
         'echo ip :: decode_ip(' . $this->get_dataspace_ref_code() . '->get("' . $this->attributes['hash_id'] . '"));');
     }
     else
+    {
+      $code->register_include(LIMB_DIR . '/core/lib/system/sys.class.php');
       $code->write_php('echo sys :: client_ip();');
+    }
   }
 }
 
