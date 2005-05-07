f0e9e07135acc109f6dc8058eeb96d0c4e4eca36
pachanga <pachanga@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2005-05-07 10:02:58 +0000

    -- get_logs.php removes empty and unnecessary lines, moves all fix records to the top -- rolling 2.3.1 release

--- php4/branches/trunk/build/get_logs.php
+++ php4/branches/trunk/build/get_logs.php
@@ -10,27 +10,70 @@ function query_svn_logs($svn_path, $to_rev, $from_rev)
 {
   echo "Querying svn...\n";
 
+  $result = array();
   $out = array();
   $cmd = "svn log {$svn_path} -r{$to_rev}:{$from_rev}";
   exec($cmd, $out);
 
-  foreach($out as $index => $string)
+  foreach($out as $string)
   {
+    //skipping empty lines
+    if(trim($string) == '')
+      continue;
+
+    //skipping ------ lines
+    if(preg_match('~^-{20,}~', $string))
+      continue;
+
+    //skipping revision info lines
+    if(preg_match('~^r\d+~', $string))
+      continue;
+
     if(preg_match('~svn\s+merge\s+-r\s*(\d+):(\d+)\s+(\S+)\s+(\S*)~', $string, $matches))
     {
       echo "Merge detected\n";
 
       $merge_out = query_svn_logs($svn_path . $matches[3], $matches[2], $matches[1]);
-      $out[$index] = "\n========= EXPANDING MERGE START =========\n" .
-                     implode("\n", $merge_out) .
-                     "\n========= EXPANDING MERGE END =========\n";
+
+      foreach($merge_out as $merge_string)
+        $result[] =  $merge_string;
+
+    }
+    else
+    {
+      $result[] = $string;
     }
   }
 
-  return $out;
+  return $result;
+}
+
+function process_logs($out)
+{
+  echo "Processing logs...\n";
+
+  $result = array();
+  $fixes = array();
+  foreach($out as $string)
+  {
+    //putting fixes to the top
+    if(preg_match('~(fixed:?\s|fix\s)~', $string))
+      $fixes[] = $string;
+    else
+      $result[] = $string;
+  }
+
+  if($fixes)
+    array_unshift($result, "\n");
+
+  foreach($fixes as $fix)
+    array_unshift($result, $fix);
+
+  return $result;
 }
 
 $out = query_svn_logs($SVN_PATH, $TO_REV, $FROM_REV);
+$out = process_logs($out);
 
 $file = "{$OUTPUT_DIR}/LOG-{$TO_REV}-{$FROM_REV}";
 $fh = fopen($file, 'w');
