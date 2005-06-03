f3016f2aa70c1b97274fb81f3335394fbaf95f09
pachanga <pachanga@f70229e8-65b9-174f-aac5-fcbd71d6fc1d>
2005-06-03 14:45:24 +0000

    -- version stuff

--- php4/branches/3.0/version.php
+++ php4/branches/3.0/version.php
@@ -51,7 +51,7 @@ define('LIMB_RELEASE_NAME', '');
 /**
  * Software build (full) date
  */
-define('LIMB_VERSION_DATE', 'June 1, 2005');
+define('LIMB_VERSION_DATE', 'June 3, 2005');
 
 /**
  * Software logo
